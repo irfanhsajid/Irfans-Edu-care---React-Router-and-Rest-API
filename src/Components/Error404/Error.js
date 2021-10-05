4c2f233f2795b08fc4547869aab615f955bba6f6
irfanhsajid <irfanhsajid@gmail.com>
2021-10-05 15:26:23 +0600

    error404 section style changed

--- src/Components/Error404/Error.js
+++ src/Components/Error404/Error.js
@@ -3,9 +3,10 @@ import './error.css'
 const Error = () => {
     return (
         <div className="container error d-flex justify-content-center align-items-center">
-            <div className="text-center text-danger px-5 py-5 rounded-3">
-                <h1>Error 404!!</h1>
-                <h3>The page you looking for is not available.</h3>
+            <div className="text-center text-danger px-5 py-5 rounded-3 shadow-lg">
+                <h1 className="fw-bold">Error 404!!</h1>
+                <h3>The page you looking for is not Found.</h3> <br />
+                <h1 className="fw-bold">*,*</h1>
             </div>
         </div>
     );
